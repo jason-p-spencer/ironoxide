ab2bae3e093bf2decc6013f9b41cc57b894b6b71
Jason Spencer <spencer.p.jason@gmail.com>
2020-08-01 17:42:52 +0000

    Added a lives and difficulty system!

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,12 +1,47 @@
 use rand::Rng;
 use std::cmp::Ordering;
 fn main() {
+    let mut lives = 0;
+    loop {
+        println!("Guessing Game | Difficulty Select:");
+        println!("1: Easy\n2: Medium\n3: Hard\n");
+        let mut difficulty_select = String::new();
+        std::io::stdin()
+            .read_line(&mut difficulty_select)
+            .expect("Failed to read the difficulty select");
+
+        let difficulty_select: u32 = match difficulty_select.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        match difficulty_select {
+            1 => {
+                lives = 9;
+                break;
+            }
+            2 => {
+                lives = 5;
+                break;
+            }
+            3 => {
+                lives = 3;
+                break;
+            }
+            _ => {
+                break;
+            }
+        }
+    }
+
+
     println!("Can you read my mind?");
 
     let secret_number = rand::thread_rng().gen_range(1,101);
     
-    loop {
+    while lives > 0 {
         println!("Guess my number between 1 and 100: ");
+        println!("Guesses remaining: {}", lives);
         let mut guess = String::new();
 
         std::io::stdin()
@@ -21,12 +56,21 @@ fn main() {
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => {
+                println!("Too small!");
+                lives -= 1;
+            }
+            Ordering::Greater => {
+                println!("Too big!");
+                lives -= 1;
+            }
             Ordering::Equal => {
                 println!("Bingo bango bongo, you did it!");
                 break;
             }
         }
+    }
+    if lives == 0 {
+        println!("You ran out of lives! Try again.");
     }
 }
